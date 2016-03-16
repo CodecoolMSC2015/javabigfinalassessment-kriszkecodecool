9d8aebff395125ac80d99c283bf3e658f2ed977b
ivancsikkrisztian <kriszke20@gmail.com>
2016-03-16 10:53:07 +0100

    Set up git, create the Person and Skill classes.

---
+++ javabigfinalassessment/src/assessment/Skill.java
@@ -0,0 +1,25 @@
+package assessment;
+
+public class Skill {
+	String name;
+	String description;
+	double rate;
+
+	public Skill(String name, String description) {
+		this.name = name;
+		this.description = description;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public double getRate() {
+		return rate;
+	}
+
+}
